b2e82da456eb09914eadecafd7c28c6dcf626712
Mojtaba Moradli <99252405+mojtabamoradli@users.noreply.github.com>
2022-11-19 15:47:20 +0330

    Update login-with-sms.js

--- Front/pages/auth/login-with-sms.js
+++ Front/pages/auth/login-with-sms.js
@@ -8,19 +8,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { loginSuccess } from "../../redux/auth/userAction";
 import axios from "axios";
 import useTitle from "../../hooks/useTitle";
-
-
+import GoogleOAuth from "./google-oauth";
+import dynamic from "next/dynamic"
+const Timer = dynamic(() => import("../../components/Timer"), { ssr: false }); // This resolve hydration problem
 
 const SMSLogin = () => {
-
-
-
-useTitle("Log in with SMS")
-
+  useTitle("Log in with SMS");
 
   const success = useRef();
   const failed = useRef();
-
+  const timer = useRef();
 
   const router = useRouter();
 
@@ -30,11 +27,16 @@ useTitle("Log in with SMS")
   const [input, setInput] = useState({ phone: "" });
   const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
+  const [loading, setLoading] = useState(false);
+
+const [startTimer, setStartTimer] = useState(false);
 
   const [SMS, setSMS] = useState(false);
-  const [code, setCode] = useState((Math.floor((Math.random() * 10000) + 10000).toString().substring(1)));
-  const [user, setUser] = useState({});
-
+  const [code, setCode] = useState(
+    Math.floor(Math.random() * 10000 + 10000)
+      .toString()
+      .substring(1)
+  );
 
   useEffect(() => {
     if (isLoggedIn) {
@@ -46,8 +48,6 @@ useTitle("Log in with SMS")
     setErrors(authentication(input, "LOGIN_WITH_SMS"));
   }, [input, touched]);
 
-
-
   const changeHandler = (event) => {
     setInput({ ...input, [event.target.name]: event.target.value });
   };
@@ -58,77 +58,60 @@ useTitle("Log in with SMS")
 
 
 
-
-
   const submitHandler = (event) => {
     event.preventDefault();
+    setStartTimer(true)
     if (!errors.phone) {
-
       axios.post(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/send_sms`, {
         to: input.phone,
         from: process.env.NEXT_PUBLIC_MELIPAYAMAK_PHONE_NUMBER,
         text: `Your Login Code is: ${code}`,
       });
-      setSMS(true)
-      success.current.textContent = `Code Sent.`;
-      failed.current.textContent = "";
+      setSMS(true);
+      success.current.textContent = `Code Sent to ${input.phone}.`;
+      failed.current.textContent = ""
       window.localStorage.setItem("SMSkey", Math.floor(Math.random() * 10 ** 21).toString(36));
       window.localStorage.setItem("SMSkeyExpireTime", new Date().getTime());
-
     } else {
       setTouched({ phone: true });
     }
   };
 
-
-  useEffect(() => {
-    if (SMS) {
-      axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/users`).then((response) => {
-        setUser(response.data.find((user) => user.phone === input.phone));
-      });
-    }
-  }, [SMS]);
-
-
   const codeHandler = () => {
-    if(code===input.code) {
-      if (window && window.navigator.onLine) {
-        if (user) {
-              if (user.status === "allowed") {
+    setLoading(true);
+    if (code === input.code) {
+      axios.get(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/auth/users`).then((response) => {
+        const user = response.data.find((user) => user.phone === input.phone);
 
-                const emailBody = `We're verifying a recent log in for ${input.phone} at ${new Date().toLocaleDateString('fa-IR-u-nu-latn', { year:'numeric',month:'2-digit',day:'2-digit'})}, ${new Date().toString().substring(15, 21)}; If you believe that this log in is suspicious, please contact: contact@mojtabamoradli.ir`;
-                axios.post(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/send_mail`, {
-                  text: emailBody,
-                  to: user.email,
-                  subject: `Successful log in for ${input.phone}`,
-                });
-                dispatch(loginSuccess(user));
-                window.localStorage.setItem("LoginTime", new Date().getTime())
-                success.current.textContent = `Login Successful.`;
-                failed.current.textContent = "";
-              } else {
-                success.current.textContent = "";
-                failed.current.textContent = "Access Denied! Please Contact: contact@mojtabamoradli.ir";
-              }
-            
-          
+        if (user.status === "allowed") {
+          dispatch(loginSuccess(user));
+          setLoading(false);
+          window.localStorage.setItem("LoginTime", new Date().getTime());
+          success.current.textContent = `Login Successful.`;
+          failed.current.textContent = "";
+
+          axios.post(`${process.env.NEXT_PUBLIC_BACKEND_BASE_URL}/send_mail`, {
+            from: '"Mojtaba Moradli" contact@mojtabamoradli.ir',
+            text: `<p style="font-size: 15px">We're verifying a recent log in for ${user.phone} at ${new Date().toLocaleDateString("fa-IR-u-nu-latn", {
+              year: "numeric",
+              month: "2-digit",
+              day: "2-digit",
+            })}, ${new Date().toString().substring(15, 21)}</br> If you believe that this log in is suspicious, please contact: contact@mojtabamoradli.ir</p>`,
+            to: user.email,
+            subject: `Successful log in for ${input.phone}`,
+          });
         } else {
           success.current.textContent = "";
-          failed.current.textContent = "Database Error! Please try logging in again.";
-          setUser({});
+          failed.current.textContent = "Access Denied! Please Contact: contact@mojtabamoradli.ir";
+          setLoading(false);
         }
-      } else {
-        success.current.textContent = "";
-        failed.current.textContent = "Please Check Your Internet Connection.";
-      }
-
+      });
     } else {
       success.current.textContent = "";
       failed.current.textContent = "Wrong Code!";
+      setLoading(false);
     }
-  }
-
-
+  };
 
   return (
     <div className={styles.container}>
@@ -139,38 +122,60 @@ useTitle("Log in with SMS")
       </div>
       <form onSubmit={submitHandler}>
         <div>
-          <p className={styles.formControlHead}>Phone Number:</p>
-          <input className={styles.formControl} type="tel" name="phone" placeholder="09" value={input.phone} onChange={changeHandler} onFocus={focusHandler} />
-          <p className={styles.errors}>{errors.phone && touched.phone && errors.phone}</p>
-
-          {SMS && <>
-          <p className={styles.formControlHead}>Code:</p>
-          <input className={styles.formControl} type="text" placeholder="XXXX" name="code"  value={input.code} onChange={changeHandler} onFocus={focusHandler} />
-          </>}
-        </div>
         
-        {!SMS && <button className={styles.btn} type="submit">Send SMS</button>}
+          {!SMS && (
+            <>
+              <p className={styles.formControlHead}>Phone Number:</p>
+              <input className={styles.formControl} type="tel" name="phone" placeholder="09" value={input.phone} onChange={changeHandler} onFocus={focusHandler} />
+              <p className={styles.errors}>{errors.phone && touched.phone && errors.phone}</p>
+            </>
+          )}
 
+          {SMS && (
+            <>
+              <p className={styles.formControlHead}>Code:</p>
+              <input className={styles.formControl} type="text" placeholder="XXXX" name="code" value={input.code} onChange={changeHandler} onFocus={focusHandler} />
+            </>
+          )}
+        </div>
 
-
-
-
+        {!SMS && (
+          <button className={styles.btn} type="submit">
+            Send SMS
+          </button>
+        )}
       </form>
-        {SMS && <button className={styles.btn} onClick={codeHandler} >Log in</button>}
+      {SMS && (
+        <button className={loading ? styles.btnLoading : styles.btn} onClick={codeHandler}>
+          {loading ? "Logging in..." : "Log in"}
+        </button>
+      )}
       <div className={styles.messageError}>
         <span className={styles.success} ref={success}></span>
         <span className={styles.failed} ref={failed}></span>
+        <span className={styles.failed} ref={timer}>{startTimer && <Timer minutes={2} seconds={0} /> }</span>
+
       </div>
 
-
-      <div className={styles.loginWith}><span>Log in with </span><Link className={styles.a} href="/auth/login">Password</Link>{" / "}<Link className={styles.a} href="/auth/login-with-otp">OTP</Link></div>
-      <div className={styles.dontHaveAnAccount}><span>Don't Have an Account? </span><Link className={styles.a} href="/auth/signup">Sign up</Link></div>
-
+      <div className={styles.loginWith}>
+        <span>Log in with </span>
+        <Link className={styles.a} href="/auth/login-with-password">
+          Password
+        </Link>
+        {" / "}
+        <Link className={styles.a} href="/auth/login-with-otp">
+          OTP
+        </Link>
+      </div>
+      <GoogleOAuth />
+      <div className={styles.dontHaveAnAccount}>
+        <span>Don't Have an Account? </span>
+        <Link className={styles.a} href="/auth/signup">
+          Sign up
+        </Link>
+      </div>
     </div>
   );
 };
-
-
-
 
 export default SMSLogin;
